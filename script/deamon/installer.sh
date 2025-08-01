f24850cc452ac3e5196a2f320dfe63ab6a0b3698
spookyMC123 <editormaster505@gmail.com>
2025-08-01 17:55:44 +0600

    Update installer.sh

--- script/deamon/installer.sh
+++ script/deamon/installer.sh
@@ -34,7 +34,10 @@ echo -e "${CYAN}${BOLD}=========================================${RESET}\n"
 # Menu
 echo -e "${YELLOW}${BOLD}Choose an installation option:${RESET}"
 echo -e "${BG_BLUE} 1 ${RESET} ${EMOJI_ARROW} Install ${BOLD}Vortex-Deamon ()${RESET}"
-echo -e "${BG_BLUE} 2 ${RESET} ${EMOJI_ARROW} Install ${BOLD}Pterodactyl 2 (GitHub, no IPv4)${RESET}"
+echo -e "${BG_BLUE} 2 ${RESET} ${EMOJI_ARROW} Install ${BOLD}skyport-daemon ()${RESET}"
+echo -e "${BG_BLUE} 3 ${RESET} ${EMOJI_ARROW} Install ${BOLD}sader-Daemon ()${RESET}"
+echo -e "${BG_BLUE} 4 ${RESET} ${EMOJI_ARROW} Install ${BOLD}hydra-deamon ()${RESET}"
+echo -e "${BG_BLUE} 5 ${RESET} ${EMOJI_ARROW} Install ${BOLD}draco-Daemon ()${RESET}"
 echo -e "${BG_BLUE} 0 ${RESET} ${EMOJI_BACK} Return to Main Panel Installer"
 
 echo -ne "\n${CYAN}${EMOJI_INPUT} Enter your choice [0-2]: ${RESET}"
@@ -49,59 +52,59 @@ case "$choice" in
     echo -e "${RED}${EMOJI_ERROR} Installation failed. Please check your network or try again.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     }
     ;;
   2)
-    echo -e "\n${GREEN}${EMOJI_START} Installing Pterodactyl 2 (GitHub version)...${RESET}"
+    echo -e "\n${GREEN}${EMOJI_START} Installing  skyport-daemon()...${RESET}"
     sleep 1
-    bash <(curl -fsSL https://raw.githubusercontent.com/spookyMC123/pterodactylpaneleasyinstall/main/ptero.se) || {
+    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/skyportdinstall.sh) || {
     echo -e "${RED}${EMOJI_ERROR} Installation failed. Please check your connection or script source.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     }
     ;;
    3)
-    echo -e "\n${GREEN}${EMOJI_START} Installing Vortex-Deamon(${EMOJI_ARROW}best but buggy)...${RESET}"
+    echo -e "\n${GREEN}${EMOJI_START} Installing sader-Daemon()...${RESET}"
     sleep 1
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/install.sh) || {
+    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/saderinstall.sh) || {
     echo -e "${RED}${EMOJI_ERROR} Installation failed. Please check your network or try again.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     }
     ;;
    4)
-    echo -e "\n${GREEN}${EMOJI_START} Installing Vortex-Deamon(${EMOJI_ARROW}best but buggy)...${RESET}"
+    echo -e "\n${GREEN}${EMOJI_START} Installing hydra-Deamon()...${RESET}"
     sleep 1
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/install.sh) || {
+    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/hydradeamon.sh) || {
     echo -e "${RED}${EMOJI_ERROR} Installation failed. Please check your network or try again.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     }
     ;;
    5)
-    echo -e "\n${GREEN}${EMOJI_START} Installing Vortex-Deamon(${EMOJI_ARROW}best but buggy)...${RESET}"
+    echo -e "\n${GREEN}${EMOJI_START} Installing Draco-Deamon()...${RESET}"
     sleep 1
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/install.sh) || {
+    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/deamon/script/dracoinstall.sh) || {
     echo -e "${RED}${EMOJI_ERROR} Installation failed. Please check your network or try again.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     }
     ;;
   0)
     echo -e "\n${YELLOW}${EMOJI_BACK} Redirecting to Main Installer...${RESET}"
     sleep 1
-    bash <(curl -fsSL https://github.com/spookyMC123/panel-installer/raw/refs/heads/main/installer.sh)
+    bash <(curl -fsSL https://raw.githubusercontent.com/Ininityforge/panel-Installer/refs/heads/main/Install.sh)
     ;;
   *)
     echo -e "\n${RED}${EMOJI_ERROR} Invalid input! Please run the script again and choose 0, 1, or 2.${RESET}"
     sleep 2
     echo -e "\n${RED}${EMOJI_ERROR}Backing${RESET}"
-    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/ptrodactyl/install.sh)
+    bash <(curl -fsSL https://github.com/spookyMC123/skipt/raw/refs/heads/main/script/Deamon/install.sh)
     ;;
 esac
 
